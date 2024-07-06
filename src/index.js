2ce382e0cbd61f059a282ba3cc65774d18164f33
Adarsh <adarshshanu3@gmail.com>
2024-07-06 16:30:23 +0530

    ksdf

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom';
 import ChatProvider from './context/ChatProvider';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <ChatProvider>
